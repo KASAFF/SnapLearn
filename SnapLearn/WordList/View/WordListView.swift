8a8cbb2c75e5ca2f855d0ffa12a519c9dc009eee
Alexey Kosov <kasaff322@icloud.com>
2024-06-18 22:05:13 +0400

    refactor WordListView

--- SnapLearn/WordList/View/WordListView.swift
+++ SnapLearn/WordList/View/WordListView.swift
@@ -1,5 +1,5 @@
 //
-//  WordListrView.swift
+//  WordListView.swift
 //  SnapLearn
 //
 //  Created by Aleksey Kosov on 12.05.2024.
@@ -11,97 +11,107 @@ import SwiftData
 struct WordListView: View {
     @Environment(\.modelContext) var modelContext
 
+    // MARK: - State Properties
+
     @State private var words = [WordModel]()
     @State private var showCardStack = false
+
+    // MARK: - Constants
+
+    enum Constants {
+        static let toLearnSectionHeader = "To learn"
+        static let learnedSectionHeader = "Learned already"
+        static let startLearningButton = "Start Learning"
+        static let navigationTitle = "My Words"
+        static let markAsLearned = "Mark as Learned"
+        static let delete = "Delete"
+        static let stillLearning = "Still learning"
+    }
+
+    // MARK: - Body
 
     var body: some View {
         NavigationView {
             VStack {
                 if showCardStack {
-                    CardStackView(
-                        words: words.filter { !$0.isLearned },
-                        onLearnAgain: { word in
-                            print("Learn Again: \(word.wordText)")
-                            word.isLearned = false
-                            words.append(word)
-                        },
-                        onSuccessfullyLearned: { word in
-                            print("Successfully Learned: \(word)")
-                            word.isLearned = true
-                            words.removeAll { $0.id == word.id }
-                        },
-                        onEndLearning: {
-                            showCardStack = false
-                        }
-                    )
+                    cardStackView
                 } else {
-                    List {
-                        Section {
-                            ForEach(words.filter { !$0.isLearned }) { word in
-                                NavigationLink(destination: WordDetailView(wordModel: word)) {
-                                    Text(word.wordText)
-                                }
-                                .swipeActions {
-                                    Button {
-                                        withAnimation(.none) {
-                                            word.isLearned = true
-                                        }
-                                    } label: {
-                                        Label("Mark as Learned", systemImage: "checkmark")
-                                    }
-                                    .tint(.green)
-                                    Button(role: .destructive) {
-                                        deleteWord(word)
-                                    } label: {
-                                        Label("Delete", systemImage: "trash")
-                                    }
-                                }
-                            }
-                        } header: {
-                            Text("To learn")
+                    wordListView
+                        .onAppear {
+                            fetchWords()
                         }
-                        Section {
-                            ForEach(words.filter { $0.isLearned }) { word in
-                                NavigationLink(destination: WordDetailView(wordModel: word)) {
-                                    Text(word.wordText)
-                                }
-                                .swipeActions {
-                                    Button("Still learning") {
-                                        withAnimation(.none) {
-                                            word.isLearned = false
-                                        }
-                                    }
-                                    .tint(.orange)
-                                    Button(role: .destructive) {
-                                        deleteWord(word)
-                                    } label: {
-                                        Label("Delete", systemImage: "trash")
-                                    }
-                                }
-                            }
-                        } header: {
-                            Text("Learned already")
-                        }
-                    }
-                    .onAppear {
-                        fetchWords()
-                    }
                 }
             }
-            .navigationTitle("My Words")
+            .navigationTitle(Constants.navigationTitle)
             .toolbar {
                 if !showCardStack {
                     Button {
                         showCardStack = true
                     } label: {
-                        Text("Start Learning")
+                        Text(Constants.startLearningButton)
                     }
                 }
             }
         }
     }
 
-    private func fetchWords() {
+    // MARK: - Subviews
+
+    private var cardStackView: some View {
+        CardStackView(
+            words: words.filter { !$0.isLearned },
+            onLearnAgain: { word in
+                word.isLearned = false
+                words.append(word)
+            },
+            onSuccessfullyLearned: { word in
+                word.isLearned = true
+                words.removeAll { $0.id == word.id }
+            },
+            onEndLearning: {
+                showCardStack = false
+            }
+        )
+    }
+
+    private var wordListView: some View {
+        List {
+            Section {
+                ForEach(words.filter { !$0.isLearned }) { word in
+                    NavigationLink(destination: WordDetailView(wordModel: word)) {
+                        Text(word.wordText)
+                    }
+                    .swipeActions {
+                        markAsLearnedButton(for: word)
+                        deleteButton(for: word)
+                    }
+                }
+            } header: {
+                Text(Constants.toLearnSectionHeader)
+            }
+            Section {
+                ForEach(words.filter { $0.isLearned }) { word in
+                    NavigationLink(destination: WordDetailView(wordModel: word)) {
+                        Text(word.wordText)
+                    }
+                    .swipeActions {
+                        stillLearningButton(for: word)
+                        deleteButton(for: word)
+                    }
+                }
+            } header: {
+                Text(Constants.learnedSectionHeader)
+            }
+        }
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension WordListView {
+
+    func fetchWords() {
         do {
             words = try modelContext.fetch(.init())
         } catch {
@@ -109,7 +119,7 @@ struct WordListView: View {
         }
     }
 
-    private func deleteWord(_ word: WordModel) {
+    func deleteWord(_ word: WordModel) {
         do {
             modelContext.delete(word)
             try modelContext.save()
@@ -118,8 +128,33 @@ struct WordListView: View {
             print(error)
         }
     }
-}
 
-#Preview {
-    WordListView()
+    func markAsLearnedButton(for word: WordModel) -> some View {
+        Button {
+            withAnimation(.none) {
+                word.isLearned = true
+            }
+        } label: {
+            Label(Constants.markAsLearned, systemImage: "checkmark")
+        }
+        .tint(.green)
+    }
+
+    func stillLearningButton(for word: WordModel) -> some View {
+        Button(Constants.stillLearning) {
+            withAnimation(.none) {
+                word.isLearned = false
+            }
+        }
+        .tint(.orange)
+    }
+
+    func deleteButton(for word: WordModel) -> some View {
+        Button(role: .destructive) {
+            deleteWord(word)
+        } label: {
+            Label(Constants.delete, systemImage: "trash")
+        }
+    }
+
 }
